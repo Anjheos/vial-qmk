e73685ccbbf688b8ebf8b96237963a0590953462
Anjheos <Anjheos.Designs@gmail.com>
2023-02-27 17:01:30 -0800

    Added Secure Vial Compatibility.

---
+++ keyboards/prototypist/pt60/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/*SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x71, 0xCA, 0x20, 0x8B, 0x36, 0xF7, 0x5D, 0xE8}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
